797dc0e1271c30d79728722cc8082bd09dad739b
wang686 <40404290+wang686@users.noreply.github.com>
2019-02-21 14:53:27 -0500

    Update A3.c

--- A3.c
+++ A3.c
@@ -11,17 +11,18 @@ Write your code in this editor and press "Run" button to compile and execute it.
 int main(){
     
     int menuSelection = 0;
+    int alpha= 0, beta = 0;//try unsigned if out put is has -1 at begeining
     char menu[] = "1. Perform Left Bit Shift Operation (one value)\n2. Perform Right Bit Shift Operation (one value)\n3. Perform Binary AND Operation (two values)\n4. Perform Binary OR Operation (two values)\n5. Perform Binary One's Complement (~) Operation (one value)\n6. Perform Binary XOR Operation (two values)\n7. Quit\n\n";
-//    scanf("%d",&menuSelection);
-//   printf("%d",menuSelection);
+    int i;
+    
 while(1){
-    printf("%s",menu);
+    printf("\n%s",menu);
     scanf("%d",&menuSelection);
     
     
     switch(menuSelection){
         case 1: 
-            printf("spmething 1");
+            printf("Enter a base 10 Integer: ");
             break;
         case 2:
             printf("spmething 2");
@@ -33,17 +34,21 @@ while(1){
             printf("spmething 4");
             break;
         case 5:
-            printf("spmething 5");
+            printf("Enter a base 10 Integer: ");
+            scanf("%d",&alpha);
+            alpha = ~alpha;
+            //printf("%d",alpha);
+            for(i = 31; i>=0; i--)
+                printf("%d",(alpha&(1<<i))>>i);
             break;
         case 6:
             printf("spmething 6");
             break;
         case 7:
-            printf("spmething 7");
-            break;
+            printf("Program finished.");
+            return;
         default:
             printf("Invalid input");
-            break;
         
         
         
