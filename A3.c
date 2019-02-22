ce945c8564a3ce4a35561612950b2bd236217fc9
wang686 <40404290+wang686@users.noreply.github.com>
2019-02-21 20:09:18 -0500

    Update A3.c

--- A3.c
+++ A3.c
@@ -9,16 +9,23 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include<string.h>
 #include <math.h>
 
-void showbits(int input);
+void showbits(int);
+void one(void);
+void two(void);
+void three(void);
+void four(void);
+void five(void);
+void six(void);
+
 int main(){
     
 
     int menuSelection = 0;
-    unsigned int alpha= 0, beta = 0;//try unsigned if out put is has -1 at begeining
+    int alpha= 0, beta = 0;
     char menu[] = "1. Perform Left Bit Shift Operation (one value)\n2. Perform Right Bit Shift Operation (one value)\n3. Perform Binary AND Operation (two values)\n4. Perform Binary OR Operation (two values)\n5. Perform Binary One's Complement (~) Operation (one value)\n6. Perform Binary XOR Operation (two values)\n7. Quit\n\n";
     int i;
     
-while(1){
+//while(1){
     printf("                Main Menu\n");
     printf("                ---------\n");
     printf("\n%s",menu);
@@ -27,31 +34,26 @@ while(1){
     
     switch(menuSelection){
         case 1: 
-            printf("Enter a base 10 Integer: ");
+            one();
             break;
         case 2:
-            printf("spmething 2");
+            two();
             break;
         case 3:
-            printf("spmething 3");
+            three();
             break;
         case 4:
-            printf("spmething 4");
+            four();
             break;
         case 5:
-            printf("Enter a base 10 Integer: ");
-            scanf("%d",&alpha);
-            alpha = ~alpha;
-            showbits(alpha);
-
-
+            five();
             break;
         case 6:
-            printf("spmething 6");
+            six();
             break;
         case 7:
             printf("Program finished.");
-            return;
+            return(0);
         default:
             printf("Invalid input");
         
@@ -59,8 +61,8 @@ while(1){
         
         
     }
-    
-}
+
+//}//while
 
 
     return 0;
@@ -71,5 +73,103 @@ void showbits(int input){
     int i;
     
     for(i = 31; i>=0; i--)
-        printf("%d",(input&(1<<i)) >>i);
+        printf("%d",abs((input&(1<<i)) >>i));
+        printf(" (base 10 %d)",input);
 }
+
+void one(){
+    int alpha, beta;
+    
+    printf("Enter a base 10 Integer: ");
+    scanf("%d",&alpha);
+    printf("\nEnter number of places to shift bit: ");
+    scanf("%d",&beta);
+    printf("\n\n        Entered Integer: ");
+    showbits(alpha);
+    alpha = alpha<<beta;
+    printf("\n        ~'ed           : ");
+    showbits(alpha);
+}
+
+void two(){
+    int alpha, beta;
+    
+    printf("Enter a base 10 Integer: ");
+    scanf("%d",&alpha);
+    printf("\nEnter number of places to shift bit: ");
+    scanf("%d",&beta);
+    printf("\n\n        Entered Integer: ");
+    showbits(alpha);
+    alpha = alpha>>beta;
+    printf("\n        ~'ed           : ");
+    showbits(alpha);
+}
+
+void three(){
+    int alpha, beta;
+    
+    printf("Enter two base 10 Integers, separated by a space: ");
+    scanf("%d",&alpha);
+    scanf("%d",&beta);
+    printf("\n\n        Entered Integer 1: ");
+    showbits(alpha);
+    printf("\n        Entered Integer 2: ");
+    showbits(beta);
+    
+    alpha = alpha & beta;
+    printf("\n        AND'ed           : ");
+    showbits(alpha);
+}
+void four(){
+    int alpha, beta;
+    
+    printf("Enter two base 10 Integers, separated by a space: ");
+    scanf("%d",&alpha);
+    scanf("%d",&beta);
+    printf("\n\n        Entered Integer 1: ");
+    showbits(alpha);
+    printf("\n        Entered Integer 2: ");
+    showbits(beta);
+    
+    alpha = alpha | beta;
+    printf("\n        OR'ed            : ");
+    showbits(alpha);
+}
+void five(){
+    int alpha;
+    
+    printf("Enter a base 10 Integer: ");
+    scanf("%d",&alpha);
+    printf("\n\n        Entered Integer: ");
+    showbits(alpha);
+    alpha = ~alpha;
+    printf("\n        ~'ed           : ");
+    showbits(alpha);
+}
+
+void six(){
+    int alpha, beta;
+    
+    printf("Enter two base 10 Integers, separated by a space: ");
+    scanf("%d",&alpha);
+    scanf("%d",&beta);
+    printf("\n\n        Entered Integer 1: ");
+    showbits(alpha);
+    printf("\n        Entered Integer 2: ");
+    showbits(beta);
+    
+    alpha = alpha ^ beta;
+    printf("\n        XOR'ed           : ");
+    showbits(alpha);
+}
+
+
+
+
+
+
+
+
+
+
+
