a08766ee78e525d51c9a1af863c3047e3e9ec0b4
wang686 <40404290+wang686@users.noreply.github.com>
2019-02-21 21:35:13 -0500

    Add files via upload

--- A3.c
+++ A3.c
@@ -1,14 +1,15 @@
-/******************************************************************************
+/*
+Filename: A3.c 
+Part of: Homework Assignment 3
+Created by: Jiaxing Wang
+Created on: 2/21/2019
+Last Modified by: Jiaxing Wang 
+Last Modified on: 2/21/2019
+*/
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 #include<stdio.h>
 #include<string.h>
-#include <math.h>
-
+#include<math.h>
 void showbits(int);
 void one(void);
 void two(void);
@@ -18,20 +19,19 @@ void five(void);
 void six(void);
 
 int main(){
-    
-
+//declare variables for future use
     int menuSelection = 0;
-    int alpha= 0, beta = 0;
+//a String of expalination of functions of the menu.
     char menu[] = "1. Perform Left Bit Shift Operation (one value)\n2. Perform Right Bit Shift Operation (one value)\n3. Perform Binary AND Operation (two values)\n4. Perform Binary OR Operation (two values)\n5. Perform Binary One's Complement (~) Operation (one value)\n6. Perform Binary XOR Operation (two values)\n7. Quit\n\n";
-    int i;
     
 while(1){
-    printf("                Main Menu\n");
+//print the menu and take input.
+    printf("\n\n\n                Main Menu\n");
     printf("                ---------\n");
     printf("\n%s",menu);
     scanf("%d",&menuSelection);
     
-    
+//switch to respond user input. It runs desired functions accordingly.    
     switch(menuSelection){
         case 1: 
             one();
@@ -53,22 +53,16 @@ while(1){
             break;
         case 7:
             printf("Program finished.");
-            return(0);
+            return(0); //exit main()
         default:
             printf("Invalid input");
-        
-        
-        
-        
-    }
+    }//switch
 
 }//while
-
-
     return 0;
-}
+}//main
 
-
+// A function to do routine work of printing bits of an integer argument.
 void showbits(int input){
     int i;
     
@@ -78,95 +72,128 @@ void showbits(int input){
         }
         else{
             printf("%d",(input&(1<<i)) >>i);
-        }//printf("%d",abs((input&(1<<i)) >>i));
+        }//not sure if  abs() is allowed. sorry for messy looking.
         printf(" (base 10 %d)",input);
 }
 
+//no parameter. simply take user input and shows left shift operation. no return value
 void one(){
-    int alpha, beta;
+    int alpha, beta; //declare varibales
     
     printf("Enter a base 10 Integer: ");
     scanf("%d",&alpha);
+
     printf("\nEnter number of places to shift bit: ");
     scanf("%d",&beta);
+
     printf("\n\n        Entered Integer: ");
     showbits(alpha);
-    alpha = alpha<<beta;
+    
+    alpha = alpha<<beta;//do as user wanted and store result in alpha
+
     printf("\n        Left Shifted   : ");
     showbits(alpha);
 }
 
+//no parameter. simply take user input and shows right shift  operation. no return value
 void two(){
-    int alpha, beta;
+    int alpha, beta; //declare varibales
     
     printf("Enter a base 10 Integer: ");
     scanf("%d",&alpha);
+
     printf("\nEnter number of places to shift bit: ");
     scanf("%d",&beta);
+
     printf("\n\n        Entered Integer: ");
     showbits(alpha);
-    alpha = alpha>>beta;
+
+    alpha = alpha>>beta;//do as user wanted and store result in alpha
+
     printf("\n        Right Shifted  : ");
     showbits(alpha);
 }
 
+//no parameter. simply take user input and shows AND operation. no return value
 void three(){
-    int alpha, beta;
+    int alpha, beta; //declare varibales
     
     printf("Enter two base 10 Integers, separated by a space: ");
     scanf("%d",&alpha);
     scanf("%d",&beta);
+
     printf("\n\n        Entered Integer 1: ");
     showbits(alpha);
     printf("\n        Entered Integer 2: ");
     showbits(beta);
     
-    alpha = alpha & beta;
+    alpha = alpha & beta;//do as user wanted and store result in alpha
+
     printf("\n        AND'ed           : ");
     showbits(alpha);
 }
+
+//no parameter. simply take user input and shows OR operation. no return value
 void four(){
-    int alpha, beta;
+    int alpha, beta; //declare varibales
     
     printf("Enter two base 10 Integers, separated by a space: ");
     scanf("%d",&alpha);
     scanf("%d",&beta);
+
     printf("\n\n        Entered Integer 1: ");
     showbits(alpha);
+
     printf("\n        Entered Integer 2: ");
     showbits(beta);
     
-    alpha = alpha | beta;
+    alpha = alpha | beta;//do as user wanted and store result in alpha
+
     printf("\n        OR'ed            : ");
     showbits(alpha);
 }
+
+//no parameter. simply take user input and shows tilde operation. no return value
 void five(){
-    int alpha;
+    int alpha; //declare varibales
     
     printf("Enter a base 10 Integer: ");
     scanf("%d",&alpha);
+
     printf("\n\n        Entered Integer: ");
     showbits(alpha);
-    alpha = ~alpha;
+
+    alpha = ~alpha;;//do as user wanted and store result in alpha
+
     printf("\n        ~'ed           : ");
     showbits(alpha);
 }
 
+//no parameter. simply take user input and shows XOR operation. no return value
 void six(){
-    int alpha, beta;
+    int alpha, beta; //declare varibales
     
     printf("Enter two base 10 Integers, separated by a space: ");
     scanf("%d",&alpha);
     scanf("%d",&beta);
+
     printf("\n\n        Entered Integer 1: ");
     showbits(alpha);
+
     printf("\n        Entered Integer 2: ");
     showbits(beta);
     
-    alpha = alpha ^ beta;
+    alpha = alpha ^ beta;//do as user wanted and store result in alpha
+
     printf("\n        XOR'ed           : ");
     showbits(alpha);
 }
+
+
+
+
+
+
 
 
 
