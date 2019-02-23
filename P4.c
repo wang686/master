e9d581d1ae3876516e9c94b60cb35df9f0da7b4d
wang686 <40404290+wang686@users.noreply.github.com>
2019-02-23 18:26:42 -0500

    Update P4.c

--- P4.c
+++ P4.c
@@ -5,16 +5,21 @@
 #include <math.h>
 #include <stdio.h>
 
-int demo_function ();		
-void DisplayStats ();		
+//function prototypes 
+int demo_function ();
+void DisplayStats ();	
 
+//a struct of a person defined with four properties
+//first name, last name, year and ppg.
 struct person{				
   char first[32];		
   char last[32];		
   int year;		
   double ppg;			
-};
+};//an important seme-colon
 
+
+//driver to demo the two functions no parameter.
 int main (void){
   printf("---------------SecondStruct-----------\n");
   demo_function ();
@@ -27,9 +32,10 @@ int main (void){
 
 //---------------SecondStruct-----------
 int demo_function (){
-  int i;
-  struct person class[54];
+  int i;//counter for a loop
+  struct person class[54];//declare an array capble of 54 people.
   
+  //init 7 people in the 7 blocks below
   class[0].year = 2004;
   class[0].ppg = 5.2;
   strcpy (class[0].first, "Jane");
@@ -65,11 +71,16 @@ int demo_function (){
   strcpy (class[6].first, "Frank");
   strcpy (class[6].last, "Frank");
   
+  //use a for-loop to call a helper function 
+  //this would show the details of each of the 7 people.
   for(i = 0; i < 7; i++){
     DisplayStats(&(class[i]));
   }
 }
 
+//a function that takes a pointer which should be struct person
+//prints the detailed info of the input
+//return no value.
 void DisplayStats(struct person *input){
 printf ("%s, %s:  year: %d points per game: %lf\n", (*input).first,(*input).last, (*input).year, (*input).ppg);
 }
