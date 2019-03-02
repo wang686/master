92f4cf059b62f562e876b7acb1b67da0d9704ec0
wang686 <40404290+wang686@users.noreply.github.com>
2019-03-01 20:48:31 -0500

    Update A4b.c

--- A4b.c
+++ A4b.c
@@ -7,37 +7,35 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 struct Node{
     int data; 
-    struct Node *next; 
+    struct Node* next; 
 };
+
 #include <stdio.h>
-void one();
-void two();
-void three();
-void four();
-void five();
-void six();
-void seven();
-void showlist();
+#include <stdlib.h>
+
+void one();void two();void three();void four();void five();void six();void seven();void showlist();
 
 int main(){
     int i,j,k;
-    int input;
+    int menu;
     int nodedata;
     
-    int data;
     struct Node *head;
+    head = (struct Node *) malloc (sizeof (struct Node));
+    head->data = NULL;
+    head->next = NULL;
     
-    char menu[] = "1. INSERT a node at the END of linklist \n2. INSERT a node at the BEGINNING of linklist \n3. DELETE a node at the END of linklist \n4. DELETE a node from the BEGINNING of linklist \n5. INSERT a node in the MIDDLE of linklist \n6. DELETE a node from the MIDDLE of linklist \n7. MODIFY any node in linklist \n8: EXIT";
+    char menulist[] = ("1. INSERT a node at the END of linklist");;// \n2. INSERT a node at the BEGINNING of linklist \n3. DELETE a node at the END of linklist \n4. DELETE a node from the BEGINNING of linklist \n5. INSERT a node in the MIDDLE of linklist \n6. DELETE a node from the MIDDLE of linklist \n7. MODIFY any node in linklist \n8: EXIT";
     
     while(1){
         //show menu, prompt user, take user input.
         printf("\n\n--------Menu-------- \n\n");
-        printf("%s\n",menu);
+        printf("%s\n",menulist);
         printf("Make your choice: ");
-        scanf("%d",&input);
+        scanf("%d",&menu);
         
         //call related functions accordingly
-        switch(input){
+        switch(menu){
             case 1:
             printf("Enter your node in the list: ");
             scanf("%d",&nodedata);
@@ -53,32 +51,26 @@ int main(){
             
             
         }
-        
-        
-        
-        
-        
     }
 
     return 0;
 }
 
-void one(struct Node *head, int input){
-    //empty list case 
-    if(head == NULL){
-        head = (struct Node *) malloc (sizeof (struct Node));
-        head->next->data = input;
-        head->next->next = NULL;
+void one(struct Node *input, int num){
+    //empty case;
+    if(input->data == NULL){
+        input->data = num;
     }
     else{
-        //find the last node and points to it
-        while(head->next != NULL){
-        head = head->next;
-        }
-        head->next = (struct Node *) malloc (sizeof (struct Node));
-        head->next->data = input;
-        head->next->next = NULL;
+    //find the last node and points to it
+    while(input->next != NULL){
+    input = input->next;
     }
+    input->next = (struct Node *) malloc (sizeof (struct Node));
+    input->next->data = num;
+    input->next->next = NULL;
+    }
+
 
 }
 void two();
