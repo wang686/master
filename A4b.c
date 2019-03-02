0055f4c62b28861ee7b08007b7d58022e2a07b3a
wang686 <40404290+wang686@users.noreply.github.com>
2019-03-01 19:48:51 -0500

    Update A4b.c

--- A4b.c
+++ A4b.c
@@ -5,7 +5,10 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-
+struct Node{
+    int data; 
+    struct Node *next; 
+};
 #include <stdio.h>
 void one();
 void two();
@@ -19,22 +22,32 @@ void showlist();
 int main(){
     int i,j,k;
     int input;
+    int nodedata;
+    
+    int data;
+    struct Node *head;
     
     char menu[] = "1. INSERT a node at the END of linklist \n2. INSERT a node at the BEGINNING of linklist \n3. DELETE a node at the END of linklist \n4. DELETE a node from the BEGINNING of linklist \n5. INSERT a node in the MIDDLE of linklist \n6. DELETE a node from the MIDDLE of linklist \n7. MODIFY any node in linklist \n8: EXIT";
     
     while(1){
+        //show menu, prompt user, take user input.
         printf("\n\n--------Menu-------- \n\n");
         printf("%s\n",menu);
         printf("Make your choice: ");
         scanf("%d",&input);
         
-        
+        //call related functions accordingly
         switch(input){
             case 1:
-            printf("one");
+            printf("Enter your node in the list: ");
+            scanf("%d",&nodedata);
+            one(head, nodedata);
+            showlist(head);
             break;
+            
+            
             case 8:
-            return;
+            return(0);
             default:
             printf("try another input");
             
@@ -50,11 +63,34 @@ int main(){
     return 0;
 }
 
-void one();
+void one(struct Node *head, int input){
+    //empty list case 
+    if(head == NULL){
+        head = (struct Node *) malloc (sizeof (struct Node));
+        head->next->data = input;
+        head->next->next = NULL;
+    }
+    else{
+        //find the last node and points to it
+        while(head->next != NULL){
+        head = head->next;
+        }
+        head->next = (struct Node *) malloc (sizeof (struct Node));
+        head->next->data = input;
+        head->next->next = NULL;
+    }
+
+}
 void two();
 void three();
 void four();
 void five();
 void six();
 void seven();
-void showlist();
+void showlist(struct Node *head){
+    while(head->next != NULL){
+        printf("%d->",head->data);
+        head = head->next;
+    }
+    printf("%d",head->data);
+}
